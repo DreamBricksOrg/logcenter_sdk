28cd76a4120812d1801d4a94d851d9ed060f1f7c
guibegotti <domingos.guilherme@gmail.com>
2026-01-07 20:11:42 -0300

    chore: Adds new python sdk files from config to middleware and the files for saving logs for late upload

--- python/log_center_sdk/__init__.py
+++ python/log_center_sdk/__init__.py
@@ -1,1 +1,6 @@
-from .log_sender import LogSender, LogSenderConfig
+from .config import LogCenterConfig
+from .sender import LogCenterSender
+from .middleware import LogCenterAuditMiddleware
+
+__all__ = ["LogCenterConfig", "LogCenterSender", "LogCenterAuditMiddleware"]
+__version__ = "0.1.1"
